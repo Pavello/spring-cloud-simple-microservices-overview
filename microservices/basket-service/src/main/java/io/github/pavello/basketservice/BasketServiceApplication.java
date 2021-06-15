a10450de7bc7034c7884e0a6fff5cfcb599c22bb
pawel.kania <pawel.kania@accenture.com>
2021-06-15 14:11:22 +0200

    base-project-commit

---
+++ microservices/basket-service/src/main/java/io/github/pavello/basketservice/BasketServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.pavello.basketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketServiceApplication.class, args);
+	}
+
+}
